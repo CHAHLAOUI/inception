4c27e10280a81deac3c6ac67384456ef8c75b2ca
CHAHLAOUI <chahlaoui101@gmail.com>
2025-10-04 13:52:50 +0100

    final

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-sleep 5
+# sleep 5
 
 
-DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
-DB_USER_PASS=$(cat /run/secrets/db_user_pass)
+# DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
+# DB_USER_PASS=$(cat /run/secrets/db_user_pass)
 
-mysql -uroot <<-EOSQL
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
-FLUSH PRIVILEGES;
+# mysql -uroot <<-EOSQL
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+# FLUSH PRIVILEGES;
 
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
-EOSQL
+# CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+# CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+# GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+# FLUSH PRIVILEGES;
+# EOSQL
 
-# Démarrer MariaDB au premier plan
-exec mysqld_safe --bind-address=0.0.0.0
+# # Démarrer MariaDB au premier plan
+# exec mysqld_safe --bind-address=0.0.0.0
