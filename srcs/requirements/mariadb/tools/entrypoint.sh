066777e3f79f1abd2c7461cfb912b0d203e15823
CHAHLAOUI <chahlaoui101@gmail.com>
2025-10-16 19:04:08 +0000

    :16-10

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-set -e
+set - xe
 
 DB_NAME=${MYSQL_DATABASE:-wordpress}
 DB_USER=${MYSQL_USER:-wp_user}
 DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
 DB_USER_PASS=$(cat /run/secrets/db_user_pass)
 
-# Vérifie si la base de données est déjà initialisée
-if [ ! -d /var/lib/mysql/mysql ]; then
-  echo "📦 Initialisation du répertoire de données MariaDB..."
+Vérifie si la base de données est déjà initialisée
   mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# if [ ! -d /var/lib/mysql/mysql ]; then
+#   echo "📦 Initialisation du répertoire de données MariaDB..."
 
-  echo "🛠️ Configuration initiale de la base..."
-  mysqld_safe --skip-networking &
-  sleep 5
+#   echo "🛠️ Configuration initiale de la base..."
+#   mysqld_safe --skip-networking &
+#   sleep 5
 
-  mysql -u root <<-EOSQL
-    ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';
+#   mysql -u root <<-EOSQL
+#     ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';
+#     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+#     GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+#     FLUSH PRIVILEGES;
+# EOSQL
+
+  # Arrêter le serveur temporaire
+#   mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
+# else
+#   echo "✅ Base de données déjà initialisée."
+# fi
+
+  << EOSQL cat > /usr/local/bin/startup
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
     GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
 
-  # Arrêter le serveur temporaire
-  mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
-else
-  echo "✅ Base de données déjà initialisée."
-fi
-
 echo "🚀 Lancement de MariaDB..."
-exec mysqld_safe --bind-address=0.0.0.0
+exec mariadbd --init-file=/usr/local/bin/startup --bind-address=0.0.0.0
